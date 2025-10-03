2d7907eb4100cf18569c421392f6581e71125371
DuproReact24 <boyreact24042k4@gmail.com>
2025-10-03 20:02:23 +0700

    Update arrow-function.js

--- huynhtantoi/demo ngay 3 10/es6 demo/arrow-function.js
+++ huynhtantoi/demo ngay 3 10/es6 demo/arrow-function.js
@@ -1,10 +1,10 @@
 console.log("arrow-function")
 
 // arrow function là một hàm khai báo mũi tên hay là hàm ẩn danh có thể dùng khai báo ngắn gọn hơn làm cho code ngắn và đẹp hơn và không có this nó sẽ lấy this từ cha khi nó được tạo bên trong
-const showName = (name)=> name // Dư 
+const showName = (name)=> name 
 const showName1 =(name) =>{
     return name
-} // dư
+} 
 
 // arrow function giữ this từ cha
 function myFunc(){
@@ -15,7 +15,7 @@ function myFunc(){
     
 }
 const db = new myFunc()
-console.log(1,db)
+
 
 // arrow function k giữ this từ cha
 function myFunc1(){
@@ -26,7 +26,7 @@ function myFunc1(){
     
 }
 const d = new myFunc1()
-console.log(2,d)
+
 
 ////
 const profile ={
@@ -36,17 +36,8 @@ const profile ={
      }
 }
 
-profile.handleClick() // dư
+profile.handleClick() 
 
-///
-const profile1 ={
-    name:"Dư",
-    handleClick: () => { 
-        console.log(this.name)
-     }
-}
-
-profile.handleClick()
  // undefind Bởi vì arrow function cần this là scope lobal nên trong object literal thì method handleClick nằm trong phạm vị của profile1 k phải khởi tạo mới nên sẽ bị undefind trường hợp đó nên sử dụng function 
 
 
