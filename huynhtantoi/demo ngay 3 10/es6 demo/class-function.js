4a5cc24d292557b3b3702eeb6fcfc496439e6f77
huynhtoi589 <huynhtoi589@gmail.com>
2025-10-04 10:21:04 +0700

    Update class-function.js

--- huynhtantoi/demo ngay 3 10/es6 demo/class-function.js
+++ huynhtantoi/demo ngay 3 10/es6 demo/class-function.js
@@ -1,10 +1,66 @@
-console.log('class function')
+console.log("=== Class Function Examples (by Tới) ===");
 
-class Person{
-    constructor(name){
+// 1. Class cơ bản
+class Person {
+    constructor(name, age) {
         this.name = name;
+        this.age = age;
     }
-    info(){
-        console.log(this.name)
+
+    speak() {
+        console.log(`Xin chào, tôi là ${this.name}, năm nay ${this.age} tuổi.`);
     }
 }
+
+const p1 = new Person("Tới", 21);
+p1.speak();
+
+// 2. Class kế thừa (extends)
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age); // gọi lại constructor của Person
+        this.major = major;
+    }
+
+    study() {
+        console.log(`${this.name} đang học ngành ${this.major}`);
+    }
+}
+
+const s1 = new Student("Tới", 21, "CNTT");
+s1.speak();   // từ Person
+s1.study();   // từ Student
+
+// 3. Class có method static (không cần new)
+class MathHelper {
+    static add(a, b) {
+        return a + b;
+    }
+}
+
+console.log("Tính 5 + 7 =", MathHelper.add(5, 7));
+
+// 4. Getter / Setter trong class
+class Product {
+    constructor(name, price) {
+        this._name = name;
+        this._price = price;
+    }
+
+    get price() {
+        return this._price + " VND";
+    }
+
+    set price(newPrice) {
+        if (newPrice > 0) {
+            this._price = newPrice;
+        } else {
+            console.log("Giá phải > 0");
+        }
+    }
+}
+
+const phone = new Product("iPhone", 20000);
+console.log(phone.price);   // getter
+phone.price = 25000;        // setter
+console.log(phone.price);
